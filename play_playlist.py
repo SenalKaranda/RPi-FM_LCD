9e8a653425c4a4312f892c6972bcebaf7211714c
Senal Karanda <55358331+SenalKaranda@users.noreply.github.com>
2023-02-08 01:50:14 -0500

    Add files via upload

---
+++ play_playlist.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+import music_tag
+import I2C_LCD_Driver
+
+mylcd = I2C_LCD_Driver.lcd()
+
+channel = 87.5
+
+folder = '/home/senal/music/mac_miller/circles'
+cd = '/home/senal/fm_transmitter'
+
+while True:
+    files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+
+    os.chdir(cd)
+
+    for file in files:
+        file_path = os.path.join(folder, file)
+        
+        mt = music_tag.load_file(file_path)
+
+        mtitle = str(mt['title'])
+        mylcd.lcd_display_string(mtitle)
+
+        process = subprocess.Popen(['./fm_transmitter', '-f', str(channel), file_path], stdout=subprocess.PIPE)
+        process.wait()
+        if process.returncode == 0:
+            mylcd.lcd_clear()
+            continue
+        else:
+            print(f"Transmission of {file} failed.")
+            exit(1)
